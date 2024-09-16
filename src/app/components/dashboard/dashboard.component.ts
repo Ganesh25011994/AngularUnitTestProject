905661ae56cf83f7438928ca901e61a5ff17e01a
Ganesh25011994 <161705710+Ganesh25011994@users.noreply.github.com>
2024-09-16 17:51:53 +0530

    feat: Adding Grid in Dashboard Component

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -19,6 +19,105 @@ export class DashboardComponent implements OnInit,AfterViewInit{
 
   chartType = ChartType.ColumnChart
 
+  chartType2 = ChartType.PieChart
+
+  pieChartData = [
+    ['London', 8136000],
+    ['New York', 8538000],
+    ['Paris', 2244000],
+    ['Berlin', 3470000],
+    ['Kairo', 19500000],
+  ]
+
+  chartType3 = ChartType.Bar
+
+  BarChartData3 = [
+    ['London', 8136000],
+    ['New York', 8538000],
+    ['Paris', 2244000],
+    ['Berlin', 3470000],
+    ['Kairo', 19500000],
+  ]
+
+zoneslist   = [
+    {
+      id: 1,
+      city: "Chennai"
+    },
+    {
+      id: 2,
+      city: "Bangalore"
+    },
+    {
+      id: 3,
+      city: "Mumbai"
+    },
+    {
+      id: 4,
+      city: "Kolkatta"
+    },
+  ];
+
+  BranchList = [
+    {
+      id: 1,
+      branchId: 101,
+      Place: "Adyar"
+    },
+    {
+      id: 1,
+      branchId: 102,
+      Place: "Adyar"
+    },
+    {
+      id: 2,
+      branchId: 201,
+      Place: "Koramangala"
+    }
+  ];
+
+  ProductList = [
+    {
+      id: 1,
+      branchId: 101,
+      ProductId: 18053,
+      Product: "Retail Loan"
+    },
+    {
+      id: 1,
+      branchId: 102,
+      ProductId: 18054,
+      Product: "Home Loan"
+    },
+    {
+      id: 2,
+      branchId: 201,
+      ProductId: 18055,
+      Product: "Vehicle Loan"
+    }
+  ]
+  
+  TestList = [
+    {
+      id: 1,
+      branchId: 101,
+      ProductId: 18053,
+      Product: "Test1"
+    },
+    {
+      id: 1,
+      branchId: 102,
+      ProductId: 18054,
+      Product: "Test2"
+    },
+    {
+      id: 2,
+      branchId: 201,
+      ProductId: 18055,
+      Product: "Test3"
+    }
+  ]
+
   initianSearchIndex = 0
   myColumns = [ChartConfiguration.SearchCriterias[this.initianSearchIndex], 'Leads Count',{role:'style'}]
 
